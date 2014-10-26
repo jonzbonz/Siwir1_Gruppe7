7a5d066edaea872ee977e55b3a3c15eb44401dba
jonzbonz <ub97eqoz@i10cip10.i10.informatik.uni-erlangen.de>
2014-10-26 14:38:22 +0100

    massive performance verbesserungen

--- Aufgabe1/matmult.cpp
+++ Aufgabe1/matmult.cpp
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#include "immintrin.h"
+#include "immintrin.h"
 
 using namespace std;
  
@@ -25,16 +25,14 @@ extern "C" {
 }
 #endif
 
-//PROBABLY 32 is best, bc its only slightly slower then 16 (which gives best performance for 1024x1024)
-//but you need much less ram then with 16
-#define THRESHOLD 16
+#define THRESHOLD 128
 #define ALIGNMENT 32 //for AVX
 
 inline void naiveMatmult(double* matA, double* matB, double* matC, int nc, int mc, int na);
 
 inline void naiveMatmultQ(double* matA, double* matB, double* matC, int nn);
 
-inline void naiveMatmultTQ(double* matA, double* matB, double* matC, int nn); //(Q for quadratic), matB has to be transposed!, currently bugged
+inline void naiveMatmultTQ(double* matA, double* matTB, double* matC, int nn);
 
 void strassenMult(double* matA, double* matB, double* matC, int nn);
 
@@ -132,12 +130,10 @@ int main(int argc, char* argv[])
 	double* matC = new double[nc*mc];
 	memset(matC, 0, nc*mc*sizeof(double));
 
-/*  
 #ifdef USE_LIKWID
 	likwid_markerInit();
 	likwid_markerStartRegion("matmult");
 #endif
-*/
 
 	
 
@@ -151,17 +147,25 @@ int main(int argc, char* argv[])
 	} else {
 		//cout << "using strassen" << endl;	
 		//Strassen-Algorithmus - nur fuer grosse und quadratische matrizen
+
+		//Transpose B
+		for(int i = 0; i < nn-1; ++i){
+			for(int j = i+1; j < nn; ++j){
+				double tmp = matB[i*nn + j];
+				matB[i*nn + j] = matB[j*nn + i];
+				matB[j*nn + i] = tmp;
+			}
+		}
+
 		strassenMult(matA, matB, matC, nn);
 	}
 
    	double time = timer.elapsed();
 
-/* 
 #ifdef USE_LIKWID
 	likwid_markerStopRegion("matmult");
 	likwid_markerClose();
 #endif  
-*/
 
 	
 	ofstream outfile(argv[3]);
@@ -189,29 +193,65 @@ inline void naiveMatmult(double* matA, double* matB, double* matC, int nc, int m
 }
 
 inline void naiveMatmultQ(double* matA, double* matB, double* matC, int nn){
-	//cout << "multiplying two " << nn << "x" << nn << " matrices..." << endl;
+	
+	//transpose matB
+	double* matBT = new double[nn*nn];
+	for(int i = 0; i < nn; ++i){
+		for(int j = 0; j < nn; ++j){
+			matBT[i*nn + j] = matB[j*nn +i];
+		}
+	}
 
+/*
 	for(int j = 0; j < nn; ++j){							// ueber die Spalten von matC
 		for(int i = 0; i  < nn; ++i){						// ueber die Zeilen von matC
 			for(int k = 0;  k  < nn; ++k){					// ueber die Spalten von matA / Zeilen von matB
-				matC[i*nn + j] += matA[i*nn + k] * matB[k*nn + j];
+				matC[i*nn + j] += matA[i*nn + k] * matBT[j*nn + k];
 			}
 		} 
 	}
+*/
+
+	double* temp = new double[4];
+	for(int j = 0; j < nn; ++j){
+		for(int i = 0; i < nn; ++i){							// ueber die Zeilen und spalten von matC
+			__m256d sum = _mm256_setzero_pd();
+			for(int k = 0; k < nn; k+=4){						// ueber die Spalten von matA / Zeilen von matB
+				__m256d A = _mm256_loadu_pd(&matA [i*nn + k]);
+				__m256d B = _mm256_loadu_pd(&matBT[j*nn + k]);
+				__m256d C = _mm256_mul_pd(A, B);
+				
+				sum = _mm256_add_pd(sum, C);
+			}
+			_mm256_storeu_pd(temp, sum);
+			matC[i*nn + j] = temp[0] + temp[1] + temp[2] + temp[3];
+		}
+	}
 }
 
-inline void naiveMatmultTQ(double* matA, double* matB, double* matC, int nn){ //(Q for quadratic), matB has to be transposed!
-	for(int i = 0; i < nn*nn*nn; ++i){
-		int zeile = i/(nn*nn);
-		int spalte = (i/nn) % nn;
-		int k = i%nn;
-		matC[zeile*nn + spalte] += matA[zeile*nn + k] * matB[spalte*nn + k];
+inline void naiveMatmultTQ(double* matA, double* matBT, double* matC, int nn){
+
+	double* temp = new double[4];
+
+	for(int j = 0; j < nn; ++j){
+		for(int i = 0; i < nn; ++i){							// ueber die Zeilen und spalten von matC
+			__m256d sum = _mm256_setzero_pd();
+			for(int k = 0; k < nn; k+=4){						// ueber die Spalten von matA / Zeilen von matB
+				__m256d A = _mm256_loadu_pd(&matA [i*nn + k]);
+				__m256d B = _mm256_loadu_pd(&matBT[j*nn + k]);
+				__m256d C = _mm256_mul_pd(A, B);
+				
+				sum = _mm256_add_pd(sum, C);
+			}
+			_mm256_storeu_pd(temp, sum);
+			matC[i*nn + j] = temp[0] + temp[1] + temp[2] + temp[3];
+		}
 	}
 }
 
 void strassenMult(double* matA, double* matB, double* matC, int nn){
 	if(nn <= THRESHOLD){
-		naiveMatmultQ(matA, matB, matC, nn);
+		naiveMatmultTQ(matA, matB, matC, nn);
 		return;
 	}
 	
@@ -225,7 +265,7 @@ void strassenMult(double* matA, double* matB, double* matC, int nn){
 	
 	double* M = new double[nnhq*7];
 
-	memset(M, 0, nnhq*7*sizeof(double));
+	//memset(M, 0, nnhq*7*sizeof(double));
 
 	double* M1 = M + nnhq*0;
 	double* M2 = M + nnhq*1;
@@ -250,19 +290,8 @@ void strassenMult(double* matA, double* matB, double* matC, int nn){
 	double* matT = new double[7*2*nnhq]; //nn = größe z.b. von matA, nnh = hälfte davon, nnhq = quadrat von nnh --> Größe von A11 z.b.
 	
 	//cout << "Matrix ist " << (7*2*nnhq*sizeof(double)/(1024*1024.0f)) << "MByte groß!" << endl;
-	memset(matT, 0, 7*2*nnhq*sizeof(double));
+	//memset(matT, 0, 7*2*nnhq*sizeof(double));
 
-	//TODO use swap operations with '^'
-	//Transpose B
-	/*
-	for(int i = 0; i < nn-1; ++i){
-		for(int j = i+1; j < nn; ++j){
-			double tmp = matB[i*nn + j];
-			matB[i*nn + j] = matB[j*nn + i];
-			matB[j*nn + i] = tmp;
-		}
-	}
-	*/
 
 	if(nnh%4 != 0){
 		cerr << "nnh ist nicht durch 4 teilbar!!! Threshold zu klein?" << endl;
@@ -275,7 +304,8 @@ void strassenMult(double* matA, double* matB, double* matC, int nn){
 	cout << matT << endl;
 	cout << M << endl;*/
 
-
+	//siwir::Timer timer;
+	
 	int pos = -1; //TODO -4 for AVX
 	for(int i = 0; i < nnh; ++i){
 		for(int j = 0; j < nnh; ++j){
@@ -284,33 +314,37 @@ void strassenMult(double* matA, double* matB, double* matC, int nn){
 			double A12 = matA[i*nn + j + nnh];
 			double A21 = matA[(i+nnh)*nn + j];
 			double A22 = matA[(i+nnh)*nn + j + nnh];
-			double B11 = matB[i*nn + j];
-			double B12 = matB[i*nn + j + nnh];
-			double B21 = matB[(i+nnh)*nn + j];
-			double B22 = matB[(i+nnh)*nn + j + nnh];
 
 			matT[0*nnhq + pos] = A11 + A22;
-			matT[1*nnhq + pos] = B11 + B22;
-
 			matT[2*nnhq + pos] = A21 + A22;
-			matT[3*nnhq + pos] =  B11;
-
 			matT[4*nnhq + pos] = A11;
-			matT[5*nnhq + pos] = B12 - B22;
-
 			matT[6*nnhq + pos] = A22;
-			matT[7*nnhq + pos] = B21 - B11;
-
 			matT[8*nnhq + pos] = A11 + A12;
-			matT[9*nnhq + pos] = B22;
-
 			matT[10*nnhq + pos] = A21 - A11;
-			matT[11*nnhq + pos] = B11 + B12;
-
 			matT[12*nnhq + pos] = A12 - A22;
-			matT[13*nnhq + pos] = B21 + B22;		
 		}	
-		
+	}
+	
+	pos = -1;
+	for(int i = 0; i < nnh; ++i){
+		for(int j = 0; j < nnh; ++j){
+			pos++;
+			double B11 = matB[i*nn + j];
+			double B21 = matB[i*nn + j + nnh];
+			double B12 = matB[(i+nnh)*nn + j];
+			double B22 = matB[(i+nnh)*nn + j + nnh];
+
+			matT[1*nnhq + pos] = B11 + B22;
+			matT[3*nnhq + pos] =  B11;
+			matT[5*nnhq + pos] = B12 - B22;
+			matT[7*nnhq + pos] = B21 - B11;
+			matT[9*nnhq + pos] = B22;
+			matT[11*nnhq + pos] = B11 + B12;
+			matT[13*nnhq + pos] = B21 + B22;		
+		}
+	}
+				
+
 		/*for(int j = 0; j < nnh; j+=4){ //AVX!
 			pos += 4;
 			//TODO align to 32
@@ -354,9 +388,13 @@ void strassenMult(double* matA, double* matB, double* matC, int nn){
 			__m256d tmp13 = _mm256_add_pd(B21, B22);
 			_mm256_storeu_pd(&matT[13*nnhq + pos], tmp13);
 		}
+		}
 		*/
-	}
 	
+   	//double time = timer.elapsed();
+	//cout << "It took " << time << "seconds" << endl;
+
+
 	//Not needed any more
 	//delete[] matA;
 	//delete[] matB;
