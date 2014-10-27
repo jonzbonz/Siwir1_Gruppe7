dc74399750a641b2f1448d6fd97ef37c459491de
Jonathan Martschinke <ub97eqoz@i10cip11.i10.informatik.uni-erlangen.de>
2014-10-28 00:45:41 +0100

    perf.log

--- Aufgabe1/matmult.cpp
+++ Aufgabe1/matmult.cpp
@@ -25,7 +25,7 @@ extern "C" {
 }
 #endif
 
-#define THRESHOLD 512
+#define THRESHOLD 1024
 #define ALIGNMENT 32 //needed for AVX
 
 inline void naiveMatmult(double* matA, double* matB, double* matC, int nc, int mc, int na);
